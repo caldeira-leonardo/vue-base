a6a0a9e8a3b0b115b397fd11193f58c11ce5ce0e
beatriztrombeta <beatriz.trombeta@youbr.tech>
2024-09-16 09:55:51 -0300

    updated about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="about">
-    <h1 class="text-3xl">Contador</h1>
-    <p></p>
+    <h1 class="text-3xl">Sobre</h1>
+    <p>
+      Esse mini projeto foi realizado com o intuito de estudar o framework Vue 3 e aplicar na
+      prática os conceitos aprendidos durante as semanas de estudo. Através do desenvolvimento da
+      lista de tarefas (to-do list), é possível consolidar conhecimentos fundamentais como o uso da
+      Composition API, manipulação de estados reativos e comunicação entre componentes.
+    </p>
+    <p>
+      Além disso, o projeto proporciona uma experiência realista no desenvolvimento de uma aplicação
+      web com operações de CRUD, permitindo ao estudante explorar funcionalidades essenciais para
+      qualquer aplicação moderna, como a adição, edição e exclusão de itens.
+    </p>
+    <p>
+      O uso de `provide` e `inject` nesse contexto reforça a capacidade de gerenciar dependências e
+      compartilhar lógica entre diferentes partes do aplicativo, facilitando o desenvolvimento de
+      aplicações escaláveis e modulares no Vue.js 3.
+    </p>
   </div>
 </template>
 
@@ -16,7 +31,7 @@
     text-align: center;
     font-family: sans-serif;
     width: 40%;
-    margin-top: 1rem ;
+    margin-top: 1rem;
   }
 }
 </style>
